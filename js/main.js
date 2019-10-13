e9bfa6cb9e124e8f915cb084e81c35fbdf30a536
Francois-Xavier <xavierdk@outlook.fr>
2019-10-13 23:36:29 +0200

    Ajout particules JS + Correction BUG NAv

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-$(document).ready(function(){
-    $('.menu-toggle').click(function(){
-        $('.menu-toggle').toggleClass('active')
-        $('nav').toggleClass('active')
-    })
-})
+$(document).ready(function() {
+  $(".menu-toggle").click(function() {
+    $(".menu-toggle").toggleClass("active");
+    $("nav").toggleClass("active");
+  });
+});
+
+let CmptDM = 0;
+document.getElementById("btndark").onclick = function() {
+  CmptDM++;
+  console.log(CmptDM);
+
+  if (CmptDM % 2 === 0) {
+    document.getElementById("btndark").innerHTML = '<i class="fas fa-moon"></i>';
+    document.body.style.background = "#131418";
+  } else {
+    document.getElementById("btndark").innerHTML = '<i class="fas fa-sun"></i>';
+    document.body.style.background = "#ffffff";
+  }
+};
